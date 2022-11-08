280c2b6a393ba5a64ce5bf370e98e3116701d441
AR-Rashik <66801782+AR-Rashik@users.noreply.github.com>
2022-11-08 20:55:41 +0600

    Project setup

---
+++ src/Pages/Router/Routes/Routes.js
@@ -0,0 +1,24 @@
+import Main from "../../../Layout/Main";
+import Blog from "../../Blog/Blog";
+import Home from "../../Home/Home/Home";
+
+const { createBrowserRouter } = require("react-router-dom");
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/blog",
+        element: <Blog></Blog>,
+      },
+    ],
+  },
+]);
+
+export default router;
